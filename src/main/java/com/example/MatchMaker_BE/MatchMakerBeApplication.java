e3a559d2185fb6d6602ac7ad52c0584df2cec09f
Team Operations <wematch-teamops@outlook.de>
2023-11-28 15:04:54 +0100

    Spring Boot init

---
+++ src/main/java/com/example/MatchMaker_BE/MatchMakerBeApplication.java
@@ -0,0 +1,13 @@
+package com.example.MatchMaker_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatchMakerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MatchMakerBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MatchMaker_BE/MatchMakerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MatchMaker_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MatchMakerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
