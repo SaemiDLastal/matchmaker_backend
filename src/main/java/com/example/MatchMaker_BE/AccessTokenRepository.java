2d8ac5ae2f722bca4a67e0dc5562c00e5d30916c
Team Operations <wematch-teamops@outlook.de>
2023-12-13 16:26:54 +0100

    Restructuring 2

--- src/main/java/com/example/MatchMaker_BE/AccessTokenRepository.java
+++
@@ -1,6 +0,0 @@
-/*ackage com.example.MatchMaker_BE;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AccessTokenRepository extends JpaRepository<String, Long>{ }*/
